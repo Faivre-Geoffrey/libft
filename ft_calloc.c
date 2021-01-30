a4c6e4661568948172cdbc44f84d8191a8d335c8
gefaivre <gefaivre@student.42.fr>
2021-01-30 12:37:53 +0100

    100%

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: gefaivre <gefaivre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/24 11:20:24 by gefaivre          #+#    #+#             */
-/*   Updated: 2020/12/02 09:44:00 by gefaivre         ###   ########.fr       */
+/*   Updated: 2020/12/07 08:57:23 by gefaivre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,9 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*result;
-	size_t	i;
 
-	i = 0;
 	if (!(result = malloc(size * nmemb)))
 		return (NULL);
-	while (((char*)result)[i])
-	{
-		((char*)result)[i] = 0;
-		i++;
-	}
+	ft_memset(result, 0, nmemb * size);
 	return (result);
 }
